5c65a1deab9f49495282f6f667758c42956f0632
kutanjun <648868272@qq.com>
2026-01-06 20:42:23 +0800

    Update homepage and base url to 8591wiki

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode, command }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: command === 'serve' ? '/' : '/gamewiki/',
+      base: command === 'serve' ? '/' : '/8591wiki/',
       server: {
         port: 3000,
         host: '0.0.0.0',
