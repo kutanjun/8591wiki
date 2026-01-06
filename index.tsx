251e331d01ef4781b2dc50ffbf00d162d75f9d91
kutanjun <648868272@qq.com>
2026-01-06 20:46:24 +0800

    Update BrowserRouter basename to 8591wiki

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/gamewiki'}>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/8591wiki'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
